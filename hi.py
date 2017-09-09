e93af88c7c5ee214e13d7164ee2c44f4d5dcb922
sergiypotapov <sergiy.potapov@gmail.com>
2017-09-09 17:06:21 +0300

    in Agregate Codes Hours add merging only those hours which are in current month #5

--- hi.py
+++ hi.py
@@ -1,1 +1,20 @@
-import xml
+import openpyxl
+wb = openpyxl.load_workbook("Luxoft_Timesheet_27_2017_Denys_LebedevV70.xlsx", data_only=True)
+
+
+ws = wb.active
+alph = 'EFGHIJKL'
+digit = '5'
+for letter in alph:
+
+    cell = letter + digit
+
+    value = str(ws[cell].value)
+    value = value[8:10]
+
+    if value == '01':
+        index = alph.index(letter)
+
+        letter = alph[index]
+        print(letter)
+
